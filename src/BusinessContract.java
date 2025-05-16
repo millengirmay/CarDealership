f150af0f98dd1d6d603d61329871af0101ab9c6a
Student <millenkahsaygirmay@gmail.com>
2025-05-15 22:12:00 -0700

    update commit

--- src/BusinessContract.java
+++ src/BusinessContract.java
@@ -8,4 +8,17 @@ public class BusinessContract {
     private double totalPrice;
     private double monthlyPayment;
     private Vehicle vehicle;
+
+    //Constructor
+    BusinessContract(Date date, String customerName, String customerEmail,
+                     boolean isSold, double totalPrice, Double monthlyPayment, Vehicle vehicle){
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerName;
+        this.isSold = isSold;
+        this.totalPrice = totalPrice;
+        this.monthlyPayment = monthlyPayment;
+        this.vehicle = vehicle;
+    }
+
 }

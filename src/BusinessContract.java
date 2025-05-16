4e6060eb0b41a5bd8c997048b9afd6ed621d5864
Student <millenkahsaygirmay@gmail.com>
2025-05-15 22:01:31 -0700

    update commit

---
+++ src/BusinessContract.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class BusinessContract {
+    private Date date;
+    private String customerName;
+    private String customerEmail;
+    private boolean isSold;
+    private double totalPrice;
+    private double monthlyPayment;
+    private Vehicle vehicle;
+}

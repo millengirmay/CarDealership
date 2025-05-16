477a06965e9579ba977731aa1c2c5253e8fcdb65
Student <millenkahsaygirmay@gmail.com>
2025-05-15 22:22:10 -0700

    update commit

--- src/BusinessContract.java
+++ src/BusinessContract.java
@@ -1,6 +1,6 @@
 import java.util.Date;
 
-public class BusinessContract {
+public abstract class BusinessContract {
     private Date date;
     private String customerName;
     private String customerEmail;
@@ -21,4 +21,49 @@ public class BusinessContract {
         this.vehicle = vehicle;
     }
 
+    public Date getDate() {
+        return date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public boolean isSold() {
+        return isSold;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public double getMonthlyPayment() {
+        return monthlyPayment;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                Date: %s
+                Name: %s
+                Email: %s
+                Sold: %s
+                Total: %.2f
+                Monthly: %.2f""",
+                this.date,
+                this,customerName,
+                this,customerEmail,
+                this.isSold ? "YES" : "NO",
+                this.totalPrice,
+                this.monthlyPayment);
+
+    }
 }

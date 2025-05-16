69cd62f9f3204fed526f59e33a5e8994007bce1a
Student <millenkahsaygirmay@gmail.com>
2025-05-15 23:01:44 -0700

    update commit

---
+++ src/ContractFileManager.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+public class ContractFileManager {
+
+    public ArrayList<BusinessContract> load(){
+
+    ArrayList<BusinessContract> list = new ArrayList<>();
+
+    String line = ""; //Read this
+    BusinessContract bc = parseContract(line);
+    list.add(bc);
+
+    return list;
+    }
+
+    public void save(ArrayList<BusinessContract> list){
+        for(BusinessContract c : list){
+            System.out.println("SAVING: " + c);
+            String line = getContractString(c);
+            System.out.println(line);
+        }
+    }
+
+    BusinessContract parseContract(String line){
+        int MIN_LENGTH = 16;
+        int CONTRACT_TYPE = 0;
+        int CUSTOMER_DATE = 1;
+        int CUSTOMER_NAME = 2;
+        int CUSTOMER_EMAIL = 3;
+
+        int VIN =4;
+        int YEAR = 5;
+        int MAKE = 6;
+        int MODEL = 7;
+        int TYPE = 8;
+        int COLOR = 9;
+        int MILES = 10;
+        int PRICE = 11;
+
+        //Begin Parsing
+        String[] parts = line.split("\\|");
+
+        if(parts.length < MIN_LENGTH){
+            System.out.println("ERROR IN LINE: " + line);
+            return null;
+        }
+
+        Vehicle vehicle = new Vehicle(
+                Integer.parseInt(parts[VIN]),
+                Integer.parseInt(parts[YEAR]),
+                parts[MAKE],
+                parts[MODEL],
+                parts[TYPE],
+                parts[COLOR],
+                Integer.parseInt(parts[MILES]),
+                Double.parseDouble(parts[PRICE])
+        );
+
+    }
+
+}

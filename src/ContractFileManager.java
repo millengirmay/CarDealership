0d5526ad4cdd69d1633e52672e681ab9ddc15509
Student <millenkahsaygirmay@gmail.com>
2025-05-16 10:57:03 -0700

    update commit

--- src/ContractFileManager.java
+++ src/ContractFileManager.java
@@ -96,8 +96,31 @@ public class ContractFileManager {
             double salesTaxAmount = Double.parseDouble(parts[SALES_TAX_AMOUNT]);
             double recordingFee = Double.parseDouble(parts[RECORDING_FEE]);
             double processingFee = Double.parseDouble(parts[PROCESSING_FEE]);
-        }
+            boolean isFinanced = parts[IS_FINANCED].equalsIgnoreCase("YES");
 
+            return new SalesContract(vehicle, date, customerName, customerEmail, isSold, totalPrice, salesTaxAmount, recordingFee, processingFee, isFinanced);
+        }else if(contractType.equalsIgnoreCase("Lease")){
+            double expectedValue = Double.parseDouble(parts[EXPECTED_ENDING_VALUE]);
+            double fee = Double.parseDouble(parts[LEASE_FEE]);
+            double combined = Double.parseDouble(parts[COMBINED]);
+            double monthly = Double.parseDouble(parts[MONTHLY_LEASE]);
+
+            return new LeaseContract(vehicle, date, customerName, customerEmail, isSold, combined, expectedValue, fee);
+        }
+        return null;
+    }
+
+    String getContractString(BusinessContract bc){
+        return "";
+    }
+
+    public static void main(String[] args){
+        ContractFileManager cfm = new ContractFileManager();
+
+        BusinessContract bc1 = cfm.parseContract("SALE|20210928|Dana Wyatt|dana@texas.com|10112|1995|Ford|Explorer|SUV|Red|525123|9595.00|49.75|100.00|295.00|1439.75|No|0.00");
+        System.out.println(bc1);
+
+        BusinessContract bc2 = cfm.parseContract("")
     }
 
 }
